4b94ebeb4a4f1703e2ab13e912010ecad335ee24
user <raphels123@gmail.com>
2022-11-04 00:33:35 -0300

    other commit

---
+++ curso_ja/src/curso_ja/Main.java
@@ -0,0 +1,13 @@
+package curso_ja;
+
+public class Main {
+
+	public static void main(String[]args) {
+		int idade = 35; //só posso armazenar numeros inteiros, pode ser o long também
+		double altura = 1.78; //só posso armazenar numeros quebrados(float)
+		char sexo = 'F'; //só posso armazenar apenas um caráctere
+		
+		
+		System.out.println(altura);
+	}
+}
