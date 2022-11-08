f4b7acf25a3ddfcec46b3da8e851e9a626962c3e
user <raphels123@gmail.com>
2022-11-07 21:16:46 -0300

    other commit

---
+++ estrutura-condicional/curso_ja/src/curso_ja/Main.java
@@ -0,0 +1,36 @@
+package curso_ja;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+//Expressões Comparativas
+	// expressão resultado valor verdade
+	//exemplo
+	// 5>10 resultado falso
+	// comparadores >,<,>=,<=,==
+	// ou seja
+	// x > 0 , verifico se x é maior que dois
+	
+	
+	
+	
+	
+	//Expressôes lógicas
+	//Operadores 
+	// && = "E"
+	// || = "OU"
+	// ! = "negação"
+	// Ideia por tras do operador 'E', ideia de junção de condições , ou seja, todas as condições devem ser verdadeiras para obtermos verdade
+	//exemplo x <= 20 && x <= 10, supondo que x = 5 , o resultado é verdadeiro
+	//Ideia por tras do operador 'OU', a ideia é de pelo menos uma deve ser verdadeira , se uma for vedade a expressão é verdade
+	// x == 10 || x > 10 , contando com x = 10 , a expressao é verdade
+	// Ideia por tras do operador nao , ideia de negação da expressao
+	// !x=10, ou seja não pode ser x = 10 , ele inverte a condição 
+
+
+	
+
+	
+}
